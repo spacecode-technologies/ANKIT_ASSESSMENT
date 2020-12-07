53f394e2d8115e95803ff29faf1cb7e3ff2b6b41
Ankit Pavar <geekyanky@gmail.com>
2020-12-07 13:47:05 +0530

    Completed Inventory page

--- assesment/src/pages/InventoryPage.js
+++ assesment/src/pages/InventoryPage.js
@@ -3,15 +3,32 @@ import { Grid, InputBase, Typography } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import { useInventoryPageStyles } from '../style';
 
+import Table from '../components/Table'
+
 const InventoryPage = () => {
   const classes = useInventoryPageStyles();
   return (
     <>
-      <Grid container spacing={2} alignItems='center'>
-        <Grid item>
+      <Grid
+        container
+        spacing={4}
+        alignItems='center'
+        style={{ marginBottom: '10px' }}>
+        <Grid item md={3}>
           <InputBase className={classes.input} endAdornment={<SearchIcon />} />
         </Grid>
+        <Grid item md={3}>
+          <Typography variant='body1'>
+            <span className={classes.typography}>Total Stones </span>: 50
+          </Typography>
+        </Grid>
+        <Grid item md={6}>
+          <Typography variant='body1'>
+            <span className={classes.typography}>Total Carats </span>: 120
+          </Typography>
+        </Grid>
       </Grid>
+      <Table />
     </>
   );
 };
---
+++ assesment/src/pages/TransactionsPage.js
@@ -0,0 +1,12 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+const TransactionsPage = () => {
+  return (
+    <div>
+      <Typography variant='h3'>Transactions Page</Typography>
+    </div>
+  );
+};
+
+export default TransactionsPage;
